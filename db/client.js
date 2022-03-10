5c5d1451ef46b6bf87bc3eb372648f3aecd4c108
AyeMonKhine <ayemonkhine123@gmail.com>
2022-03-09 23:28:14 -0800

    created product table with dropping table

--- db/client.js
+++ db/client.js
@@ -1,12 +1,14 @@
 // Connect to DB
-const { Client } = require('pg');
+
+const { Client } = require("pg");
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'univ-boilerplate-test';
+const DB_NAME = "univ-boilerplate-test";
 
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
+console.log("... DB URL", DB_URL);
 const client = new Client(DB_URL);
 
 module.exports = client;
--- db/init_db.js
+++ db/init_db.js
@@ -11,11 +11,13 @@ async function buildTables() {
     // drop tables in correct order
     console.log("Dropping all tables...");
     await client.query(`
-    DROP TABLE IF EXISTS reviews; 
-    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS users cascade;
+    DROP TABLE IF EXISTS reviews cascade; 
+    DROP TABLE IF EXISTS products cascade;
+    
     `);
     console.log("Finished dropping tables...");
-
+    // DROP TABLE IF EXISTS users;
     // build tables in correct order
     console.log("Starting to build tables...");
     await client.query(`
@@ -23,15 +25,24 @@ async function buildTables() {
     CREATE TABLE users(
       id SERIAL PRIMARY KEY,
       username VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
+      password VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
       "isAdmin" BOOLEAN DEFAULT false
     );
 
+    CREATE TABLE products(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL,
+      price DECIMAL(10,2),
+      inventoryQTY INTEGER,
+      category VARCHAR(255) UNIQUE NOT NULL,
+      productImg VARCHAR(255) not null
+    );
 
     CREATE TABLE reviews(
       id SERIAL PRIMARY KEY,
-      "userId" INTEGER REFERENCES user(id) NOT NULL, --will use the creator of comment as a ref
+      "userId" INTEGER REFERENCES users(id) NOT NULL, --will use the creator of comment as a ref
       "productId" INTEGER REFERENCES products(id) NOT NULL, --will use the product id number as a ref
       rating INTEGER,
       review TEXT NOT NULL
@@ -49,11 +60,36 @@ async function populateInitialUsers() {
   console.log("Starting to populate users...");
   try {
     const usersToPopulate = [
-      { username: "bigrubes", password: "fullstack1" , email: "bigrubes@gmail.com", isAdmin: false },
-      { username: "numnum", password: "fullstack2" , email: "numnum@gmail.com", isAdmin: false },
-      { username: "kaiser", password: "fullstack3" , email: "kaiser@gmail.com", isAdmin: false },
-      { username: "daisyduck", password: "fullstack4" , email: "daisyduck@gmail.com", isAdmin: false },
-      { username: "remote", password: "controllers" , email: "remotecontrollers@gmail.com", isAdmin: true },
+      {
+        username: "bigrubes",
+        password: "fullstack1",
+        email: "bigrubes@gmail.com",
+        isAdmin: false,
+      },
+      {
+        username: "numnum",
+        password: "fullstack2",
+        email: "numnum@gmail.com",
+        isAdmin: false,
+      },
+      {
+        username: "kaiser",
+        password: "fullstack3",
+        email: "kaiser@gmail.com",
+        isAdmin: false,
+      },
+      {
+        username: "daisyduck",
+        password: "fullstack4",
+        email: "daisyduck@gmail.com",
+        isAdmin: false,
+      },
+      {
+        username: "remote",
+        password: "controllers",
+        email: "remotecontrollers@gmail.com",
+        isAdmin: true,
+      },
     ];
     const users = await Promise.all(usersToPopulate.map(populateUser));
 
