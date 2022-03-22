48ae47732c9584003f0f9c5fd1d6820a9d380295
Bryan Num <bnum33@gmail.com>
2022-03-21 18:16:00 -0700

    initial designs for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "../style/Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>Placholder Footer</h1>
+      <div className="footer-col1">
+        <h2 className="footer-title">Join The Adventure</h2>
+        <p className="footer-content">
+          Recieve updates on exclusive offers and events as they come!
+        </p>
+      </div>
+      <div className="footer-col2">
+        <h2 className="footer-title">Get Help</h2>
+        <p className="footer-content">Order Status</p>
+        <p className="footer-content">Delivery</p>
+        <p className="footer-content">Returns</p>
+        <p className="footer-content">Payment Options</p>
+        <p className="footer-content">Contact Us</p>
+      </div>
+      <div className="footer-col3">
+        <h2 className="footer-title">About Us</h2>
+        <p className="footer-content">News</p>
+        <p className="footer-content">Blog</p>
+        <p className="footer-content">Privacy Policy</p>
+        <p className="footer-content">Terms of Service</p>
+      </div>
+      <div className="footer-links"></div>
     </div>
   );
 };
