4f5c080b05327b61b17ee64f41ad96d0060ee27f
AyeMonKhine <ayemonkhine123@gmail.com>
2022-03-23 10:33:02 -0700

    product in progress commit

---
+++ src/components/Product.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import "../style/Product.css";
+
+return (
+  <div className="product-container">
+    <div className="product-header"></div>
+  </div>
+);
---
+++ src/components/ProductImage.js
@@ -0,0 +1,3 @@
+import React from "react";
+import cardplaceholder from "../imgs/cardplaceholder.png";
+import "../style/ProductImage.css";
