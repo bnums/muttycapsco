66d3dd8a56b944354a8095b121d1f42079f3b798
Bryan Num <bnum33@gmail.com>
2022-03-26 11:35:35 -0700

    resolving conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, Header, Footer } from "./";
 import "../style/App.css";
-<<<<<<< HEAD
-import ReviewsForm from "./ReviewsForm";
-=======
-import Temp from "./Temp";
 import Product from "./Product";
->>>>>>> edb245bd411495d9963c7e654b28f5c13139e928
 
 const App = () => {
   return (
@@ -15,15 +10,10 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="/review-form/:productId" element={<ReviewsForm />} />
-=======
-        <Route path="/testpage" element={<Temp />} />
         <Route
           path="/product"
           element={<Product name={"Testing"} price={100} />}
         />
->>>>>>> edb245bd411495d9963c7e654b28f5c13139e928
       </Routes>
       <Footer />
     </div>
